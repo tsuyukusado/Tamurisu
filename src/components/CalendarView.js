e0f8591ec44d09fe23d2920718b9600a62ed7b7e
Tsuyukusa Kureyon <kureyon90454@gmail.com>
2025-06-20 03:15:01 +0900

    カレンダーフルサイズ変更前

--- src/components/CalendarView.js
+++ src/components/CalendarView.js
@@ -3,24 +3,30 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
-function CalendarView({ tasks, onDateSelect }) {
-const events = (tasks || [])
-  .filter((task) => !!task.dueDate)
-  .map((task) => ({
-    title: task.title,
-    date: task.dueDate,
-  }));
-  return (
-    <div>
-      <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
-        initialView="dayGridMonth"
-        events={events}
-        dateClick={(info) => onDateSelect(info.dateStr)}
-        height="auto"
-      />
-    </div>
-  );
+function CalendarView({ tasks = [], onDateSelect }) {
+  const events = tasks
+    .filter((task) => !!task.dueDate)
+    .map((task) => ({
+      title: task.title,
+      date: task.dueDate,
+    }));
+
+return (
+  <div className="calendar-wrapper">
+<FullCalendar
+  plugins={[dayGridPlugin, interactionPlugin]}
+  initialView="dayGridMonth"
+  events={events}
+  dateClick={(info) => onDateSelect(info.dateStr)}
+  height="auto"
+  headerToolbar={{
+    left: "title",         // ← 空にする
+    center: "",  // ← 〜
+    right: "prev,next today" // ← ボタンを右側に追いやる
+  }}
+/>
+  </div>
+);
 }
 
 export default CalendarView;
